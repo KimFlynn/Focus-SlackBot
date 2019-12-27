21780f7166d818a2cfcfa1d96667b3ae8b0be09c
Kim Flynn <43191440+KimFlynn@users.noreply.github.com>
2019-12-27 05:32:07 -0800

    auth token edit out

--- RealTimeMessagingSys.py
+++ RealTimeMessagingSys.py
@@ -1,7 +1,7 @@
 from slackclient import SlackClient
 import time
  
-slack_client = SlackClient('xoxb-873400785666-885730484036-Cgw6ApEfj8IxWoGe7SS8XMTs')
+slack_client = SlackClient('xoxb***')
  
 if slack_client.rtm_connect(with_team_state=False):
     print ("Successfully connected, listening for events")
